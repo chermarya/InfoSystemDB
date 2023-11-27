ecee63326d57ca4c630036033248d59365b3ba80
chermarya <chermarya25@gmail.com>
2023-11-27 21:26:35 +0200

    additing order full

--- InfoSystemDB/InfoSystemDB/windows/OrderSettings.xaml.cs
+++ InfoSystemDB/InfoSystemDB/windows/OrderSettings.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,7 +16,7 @@ namespace InfoSystemDB
         private ListItem[] discounts = new ListItem[VsInsideDBEntities.Content().Discount.ToList().Count];
         private ListItem[] shops;
         private ListItem[] addresses;
-        
+
         private int address_id;
         private int delivery_id;
         private int discount_id;
@@ -52,10 +53,11 @@ namespace InfoSystemDB
 
                 ReadValues();
 
-                string expr = "INSERT INTO SetOrder (ddate, delivery_id, summ, discount_id, prepay, amount_due, invoice, " +
-                              "stat, note, shop_id) VALUES (@ddate, @delivery_id, @summ, @discount_id, @prepay, " +
-                              "@amount_due, @invoice, @stat, @note, @shop_id)";
-                
+                string expr =
+                    "INSERT INTO SetOrder (ddate, delivery_id, summ, discount_id, prepay, amount_due, invoice, " +
+                    "stat, note, shop_id) VALUES (@ddate, @delivery_id, @summ, @discount_id, @prepay, " +
+                    "@amount_due, @invoice, @stat, @note, @shop_id)";
+
                 new DoSql(expr, new SqlParameter[]
                 {
                     new SqlParameter("@ddate", data),
@@ -69,6 +71,12 @@ namespace InfoSystemDB
                     new SqlParameter("@note", note),
                     new SqlParameter("@shop_id", shop_id),
                 }).ToExecuteQuery();
+
+                SetProducts();
+
+                MessageBox.Show("Замовлення сформовано успішно.");
+                
+                Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive).Close();
             }
         }
 
@@ -80,14 +88,14 @@ namespace InfoSystemDB
                 if (i.Title == selAddressStr)
                     address_id = i.Id;
             }
-                
+
             string selDiscountStr = DiscountList.SelectedItem.ToString();
             foreach (ListItem i in discounts)
             {
                 if (i.Title == selDiscountStr)
                     discount_id = i.Id;
             }
-                
+
             string selShopStr = ShopList.SelectedItem.ToString();
             foreach (ListItem i in shops)
             {
@@ -102,7 +110,7 @@ namespace InfoSystemDB
                     new SqlParameter("@buyer", selectedBuyer.buyer_id),
                     new SqlParameter("@address", address_id)
                 }).ToReadQuery();
-                
+
             while (reader.Read())
             {
                 foreach (Delivery i in VsInsideDBEntities.Content().Delivery.ToList())
@@ -113,16 +121,38 @@ namespace InfoSystemDB
             }
         }
 
-        private void ReadProducts()
+        private void SetProducts()
         {
-            
+            SqlDataReader reader = new DoSql("SELECT TOP 1 order_id FROM SetOrder ORDER BY order_id DESC", new SqlParameter[] { }).ToReadQuery();
+
+            int order_id = 0;
+            int[] prod_ids = new int[selectedList.Count];
+
+            while (reader.Read())
+            {
+                order_id = reader.GetInt32(0);
+            }
+
+            for (int i = 0; i < prod_ids.Length; i++)
+            {
+                prod_ids[i] = selectedList[i].product_id;
+            }
+
+            foreach (Product i in selectedList)
+            {
+                new DoSql("INSERT INTO Packaging (order_id, product_id) VALUES (@order, @prod)", new SqlParameter[]
+                {
+                    new SqlParameter("@order", order_id),
+                    new SqlParameter("@prod", i.product_id)
+                }).ToExecuteQuery();
+            }
         }
 
         private bool Validate()
         {
             if (DateInput.Text == "" || ShopList.SelectedItem == null || AddressList.SelectedItem == null)
                 return false;
-            
+
             return true;
         }
 
@@ -172,7 +202,7 @@ namespace InfoSystemDB
             double order = Convert.ToDouble(OrderSumOutput.Text);
             double per = 0;
             string selStr = DiscountList.SelectedItem.ToString();
-            
+
             foreach (ListItem i in discounts)
             {
                 if (i.Title == selStr)
@@ -227,15 +257,16 @@ namespace InfoSystemDB
         {
             if (DGBuyers.SelectedIndex == -1)
                 return;
-            
+
             SurnameLabel.Visibility = Visibility.Collapsed;
             SurnameInput.Visibility = Visibility.Collapsed;
             DGBuyers.Visibility = Visibility.Collapsed;
             TBOutputBuyer.Visibility = Visibility.Visible;
 
             selectedBuyer = (Buyer)DGBuyers.SelectedItem;
-            
-            TBOutputBuyer.Text = "Нікнейм:\t" + selectedBuyer.nick + "\nПрізвище:\t" + selectedBuyer.surname + "\nІм'я:\t\t" +
+
+            TBOutputBuyer.Text = "Нікнейм:\t" + selectedBuyer.nick + "\nПрізвище:\t" + selectedBuyer.surname +
+                                 "\nІм'я:\t\t" +
                                  selectedBuyer.nname + "\nТелефон:\t" + selectedBuyer.tel;
 
             SetAddresses();
@@ -253,7 +284,7 @@ namespace InfoSystemDB
             }
 
             DGSelectedProducts.ItemsSource = selectedList;
-            
+
             int sum = 0;
 
             foreach (Product i in selectedList)
@@ -285,13 +316,13 @@ namespace InfoSystemDB
                         addressList.Add(i);
                 }
             }
-            
-            
+
+
             addresses = new ListItem[addressList.Count];
 
             for (int i = 0; i < addressList.Count; i++)
             {
-                addresses[i] = new ListItem(addressList[i].address_id, addressList[i].city + " " 
+                addresses[i] = new ListItem(addressList[i].address_id, addressList[i].city + " "
                     + addressList[i].dep + " (" + addressList[i].note + ")");
                 AddressList.Items.Add(addresses[i].Title);
             }
